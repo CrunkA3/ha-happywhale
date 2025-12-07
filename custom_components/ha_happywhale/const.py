91c18a9054ccf63a76ac7d9cd6077b61697b8fc3
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2025-12-07 19:34:58 +0100

    Add constants for Happywhale integration

---
+++ custom_components/ha_happywhale/const.py
@@ -0,0 +1,8 @@
+from homeassistant.const import Platform
+
+DOMAIN = 'happywhale_tracker'
+PLATFORMS = [Platform.DEVICE_TRACKER]
+MANUFACTURER = "Happywhale"
+MODEL = "Humpback"
+NAME = "Humpback Whale"
+CONF_WHALE_ID = 'whale_id'
